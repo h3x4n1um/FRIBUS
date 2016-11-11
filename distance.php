3c5289e94139a1e92b8ee0ffe1529feee416d4ce
oobol2000oo <boly456@gmail.com>
2016-11-11 23:41:55 +0700

    Implement geocoder user current city and render a fusion table

--- distance.php
+++ distance.php
@@ -29,32 +29,63 @@
       var map;
       var infoWindow;
       var pos = new google.maps.LatLng(10.0272537, 105.7698039);
+
       function initialize(){
         map = new google.maps.Map(document.getElementById('map'), {
           center: pos,
           zoom: 13
         });
-        var FusionTablesLayer = new google.maps.FusionTablesLayer({
-          map: map,
-          query: { 
-            from: '1Nncas7Hwf6w0NjFLRatQbHnqWm0oUbOT0Y6bzFjS',
-            id: '5'
-          },
-          styles: [{
-            polygonOptions: {
-              strokeWeight: "10",
-              strokeColor: "#FF0000",
-              strokeOpacity: "0.1",
-              fillOpacity: "0.0",
-              fillColor: "#000000"
-            }
-          }],
-          
-        });
+        infoWindow = new google.maps.InfoWindow({map: map});
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(
+            function(position){
+              pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              }
+              map.setCenter(pos);
+              infoWindow.setPosition(pos);
+              infoWindow.setContent('Vị trí của bạn.');
+              var Geocoder = new google.maps.Geocoder();
+              Geocoder.geocode({location: pos}, function(GeocoderResult, GeocoderStatus){
+                if (GeocoderStatus == google.maps.GeocoderStatus.OK){
+                  for (var i = 0; i < GeocoderResult[0].address_components.length; ++i){
+                    for (var j = 0; j < GeocoderResult[0].address_components[i].types.length; ++j){
+                      if (GeocoderResult[0].address_components[i].types[j] == "administrative_area_level_1"){
+                        var city = GeocoderResult[0].address_components[i];
+                        break;
+                      }
+                    }
+                  }
+                }
+                var FusionTablesLayer = new google.maps.FusionTablesLayer({
+                  map: map,
+                  query: { 
+                    from: '1AZc6fOIRth4r2iyONZWiFnj4qhmjLVqrFPH6QBT2'
+                  }
+                });
+              });
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter())
+            });
+        }
+        else{
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
       }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Lỗi: Định vị thất bại.':
+                              'Lỗi: Trình duyệt của bạn không hỗ trợ định vị.');
+        if (browserHasGeolocation) alert('Geolocation HTML5 qua giao thức HTTP không hỗ trợ bởi trình duyệt của bạn. Xem thêm tại https://goo.gl/rStTGz')
+      }
+
       google.maps.event.addDomListener(window, 'load', function(){
         initialize();
       });
+
       function incomplete(){
           alert("Under construction :)");
       }
