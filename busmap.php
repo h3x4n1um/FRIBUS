613566a6585d4e4dedbd619de9572ee64d224af9
oobol2000oo <boly456@gmail.com>
2016-12-09 00:41:53 +0700

    Implement round rendering bus station

--- busmap.php
+++ busmap.php
@@ -32,13 +32,18 @@
       var PlaceService;
       var DirectionsService = new google.maps.DirectionsService();
       var DirectionsRenderer = new google.maps.DirectionsRenderer();
+
       function initialize(){
         map = new google.maps.Map(document.getElementById('map'), {
           center: pos,
           zoom: 17
         });
-        DirectionsRenderer.setMap(map);
+        DirectionsRenderer.setOptions({
+          map: map,
+          suppressMarkers: true
+        });
         infoWindow = new google.maps.InfoWindow({map: map});
+
         /*HTML5 geolocation.*/
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
@@ -54,12 +59,14 @@
               infoWindow.setContent('Vị trí của bạn.');
               /*Reset map center*/
               map.setCenter(pos);
+
               /*Search bus station*/
               var request = {
                 location: pos,
                 rankBy: google.maps.places.RankBy.DISTANCE,
                 types: ['bus_station']
               };
+
               PlaceService = new google.maps.places.PlacesService(map);
               PlaceService.nearbySearch(request, callback);
             }, function() {
@@ -71,28 +78,49 @@
           handleLocationError(false, infoWindow, map.getCenter());
         }
       }
+
       function callback(PlaceResult, PlacesServiceStatus) {
         if (PlacesServiceStatus == google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < PlaceResult.length; ++i) {
-            createMarker(PlaceResult[i]);
-          }
-          /*Create waypoint*/
-          var request = {
-            origin: pos,
-            destination: PlaceResult[0].geometry.location,
-            travelMode: google.maps.DirectionsTravelMode.WALKING
-          }
-          DirectionsService.route(request,
-            function(DirectionsResult, DirectionsStatus) {
-              if (DirectionsStatus ==  google.maps.DirectionsStatus.OK){
-                console.log(DirectionsResult);              
-                DirectionsRenderer.setDirections(DirectionsResult);
-                alert('Bến xe bus gần nhất cách bạn ' + DirectionsResult.routes[0].legs[0].distance.text);
-              }
-            }
-          );
+          createWaypoint(PlaceResult, 0);
         } else alert('Không có bến xe bus nào gần bạn vào lúc này :(');
       }
+
+      function createWaypoint(PlaceResult, i){
+        /*Create waypoint request*/
+        var request = {
+          origin: pos,
+          destination: PlaceResult[i].geometry.location,
+          travelMode: google.maps.DirectionsTravelMode.WALKING
+        }
+
+        DirectionsService.route(request,
+          function(DirectionsResult, DirectionsStatus) {
+            if (DirectionsStatus == google.maps.DirectionsStatus.OK){
+              /*Log result*/
+              console.log(DirectionsResult);
+              /*Render waypoint*/
+              DirectionsRenderer.setDirections(DirectionsResult);
+              /*Tell user*/
+              alert('Bến xe bus cách bạn ' + DirectionsResult.routes[0].legs[0].distance.text);
+
+              var marker = new google.maps.Marker({
+                position: PlaceResult[i].geometry.location,
+                map: map,
+                title: 'Click để chuyển sang bến khác'
+              });
+
+              marker.addListener('click',
+                function(){
+                  marker.setMap(null);
+                  if (i + 1 + 1 < PlaceResult.length) createWaypoint(PlaceResult, i + 1);
+                  else createWaypoint(PlaceResult, 0);
+                }
+              );
+            }
+          }
+        );
+      }
+
       /*Create maker*/
       function createMarker(place) {
         var placeLoc = place.geometry.location;
@@ -100,26 +128,29 @@
           map: map,
           position: place.geometry.location
         });
-        var content='<strong style="font-size:1.2em">'+place.name+'</strong>'+
-                    '<br/><strong>Latitude:</strong>'+placeLoc.lat()+
-                    '<br/><strong>Longitude:</strong>'+placeLoc.lng()+
-                    '<br/><strong>Type:</strong>'+place.types[0]+
-                    '<br/><strong>Rating:</strong>'+(place.rating||'n/a');
-        var more_content='<img src="https://web.archive.org/web/20131219005116/http://googleio2009-map.googlecode.com/svn-history/r2/trunk/app/images/loading.gif"/>';
+        var content = '<strong style="font-size:1.2em">' + place.name + '</strong>' + '<br/><strong>Latitude:</strong>' + placeLoc.lat() + '<br/><strong>Longitude:</strong>' + placeLoc.lng() + '<br/><strong>Type:</strong>'+place.types[0] + '<br/><strong>Rating:</strong>' + (place.rating||'n/a');
+        var more_content = '<img src="https://web.archive.org/web/20131219005116/http://googleio2009-map.googlecode.com/svn-history/r2/trunk/app/images/loading.gif"/>';
+
         /*make a request for further details*/
-        PlaceService.getDetails({reference:place.reference}, function (place, status){
-          if (status == google.maps.places.PlacesServiceStatus.OK){
-            more_content='<hr/><strong><a href="'+place.url+'" target="details">Details</a>';
-            if(place.website){
-              more_content+='<br/><br/><strong><a href="'+place.website+'" target="details">'+place.website+'</a>';
+        PlaceService.getDetails({reference:place.reference},
+          function (place, status){
+            if (status == google.maps.places.PlacesServiceStatus.OK){
+              more_content = '<hr/><strong><a href="' + place.url + '" target="details">Details</a>';
+              if(place.website){
+                more_content += '<br/><br/><strong><a href="' + place.website + '" target="details">'+place.website + '</a>';
+              }
             }
           }
-        });
-        google.maps.event.addListener(marker, 'click', function(){
-          infowindow.setContent(content+more_content);
-          infowindow.open(map, this);
-        });
+        );
+
+        google.maps.event.addListener(marker, 'click',
+          function(){
+            infowindow.setContent(content+more_content);
+            infowindow.open(map, this);
+          }
+        );
       }
+
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
@@ -127,6 +158,7 @@
                               'Lỗi: Trình duyệt của bạn không hỗ trợ định vị.');
         if (browserHasGeolocation) alert('Geolocation HTML5 qua giao thức HTTP không hỗ trợ bởi trình duyệt mã nguồn Chronium. Xem thêm tại https://goo.gl/rStTGz')
       } 
+
       google.maps.event.addDomListener(window, 'load', function(){
         initialize();
       });
